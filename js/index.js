84688aeec58b430dc5a86823ae9dcde7a9ad04bc
Lao-Liu233 <laoliu735@gmail.com>
2020-09-14 18:29:48 +0800

    beta 1

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        menu: null,
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: [],
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        css3: true,
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['mainpage', 'index', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13', 'ending']
+        },
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
+
+        //events
+        onLeave: function (index, nextIndex, direction) {},
+        afterLoad: function (anchorLink, index) {},
+        afterRender: function () {},
+    });
+});
